c638fce9043d629ff76878682f984bceef5df07c
JonasGreim <jonas.greim@wtl.de>
2022-05-21 21:23:45 +0200

    change postgres connection to Heroku postgres and add gitignore for env file

--- db.py
+++ db.py
@@ -1,24 +1,30 @@
+import os
+from dotenv import load_dotenv
 # https://www.psycopg.org/docs/usage.html
 import psycopg2
 
+load_dotenv()
 conn = psycopg2.connect(
-    database="postgres",
-    user="postgres",
-    password="postgres",
-    host="localhost"
+    database=os.getenv('POSTGRES_DB'),
+    user=os.getenv('POSTGRES_USER'),
+    password=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('POSTGRES_HOST'),
 )
 
 # Open cursor to perform database operation
 cur = conn.cursor()
 
-cur.execute('''CREATE TABLE test3(name VARCHAR(255), age VARCHAR(255), id INT GENERATED ALWAYS AS IDENTITY);''')
-# open the csv file into the table from line 2
-with open('test.csv', 'r') as f:
-    next(f)  # Skip the header row.
-    cur.copy_from(f, 'test3', sep=',', columns=('name', 'age'))  # f=csv , <database name>, Comma-Seperated
-    conn.commit()
-    conn.close()
-f.close()
+# create table
+# cur.execute('''CREATE TABLE feedback(sentence VARCHAR(511), probabilityRight SMALLINT, guessedLanguage VARCHAR(63), userFeedback BOOLEAN, id INT GENERATED ALWAYS AS IDENTITY);''')
+# conn.commit()
+
+# get data
+# cur.execute('''SELECT * FROM test3;''')
+# print(cur.fetchall())
+
+# set data
+# cur.execute("INSERT INTO test3 (name, age) VALUES ('Jonas', 24)")
+# conn.commit()
 
 # Close communications with database
 cur.close()
