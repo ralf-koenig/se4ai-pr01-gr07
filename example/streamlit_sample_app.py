43d4429f19fd077e9e272c2c00855b8dc5f145cb
ralf-koenig <ralf.koenig@studserv.uni-leipzig.de>
2022-05-22 18:05:09 +0200

    Kleine Änderungen an der Formatierung. Keine funktionalen Änderungen.

--- example/streamlit_sample_app.py
+++ example/streamlit_sample_app.py
@@ -14,13 +14,17 @@ DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+
 @st.cache
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+    def lowercase(x):
+        return str(x).lower()
+
+    webdata = pd.read_csv(DATA_URL, nrows=nrows)
+    webdata.rename(lowercase, axis='columns', inplace=True)
+    webdata[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return webdata
+
 
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
@@ -31,7 +35,7 @@ if st.checkbox('Show raw data'):
     st.write(data)
 
 st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
 st.bar_chart(hist_values)
 
 # Some number in the range 0-23
