0cce520f057b8e3f87ad9b34538353ab8b7bc8f6
ralf-koenig <ralf.koenig@studserv.uni-leipzig.de>
2022-05-22 22:54:21 +0200

    SQL statements for database

---
+++ sql/db_create_statements.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA IF NOT EXISTS language_identification;
+
+CREATE TABLE language_identification.feedback
+(
+    id                         serial primary key,
+    text_from_user_input       text not null,
+    language_by_classifier     VARCHAR(10) not null,
+    probability_by_classifier  numeric not null,
+    language_suggested_by_user VARCHAR(10) not null,
+    creation_timestamp         timestamp not null default current_timestamp
+)
