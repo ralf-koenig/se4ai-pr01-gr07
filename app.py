d544f50b283c8f87dc2b5766144b4b6a2ec420a3
ralf-koenig <ralf.koenig@studserv.uni-leipzig.de>
2022-05-23 14:08:23 +0200

    main() split to individual functions

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ from sklearn import preprocessing
 import os
 import psycopg2
 
+# define two constants
+lang_labels = {"es": "Spanish", "en": "English", "de": "German"}
+lang_list = ["es", "en", "de"]
+
 
 # TODO: change to use inference pipeline here from the language_identification package
 # import language_identification.inference
@@ -28,7 +32,7 @@ def load_model(filename: str):
         return None
     else:
         # Load the model
-        model_directory = "model"
+        model_directory = "model/huggingface"
         path = os.path.join(model_directory, filename)
 
         try:
@@ -39,7 +43,7 @@ def load_model(filename: str):
 
 
 # cannot be cached due to object type, would need hash method for caching in streamlit
-def create_vectorize_layer(lang_list):
+def create_vectorize_layer():
     """
     Creates the vectorize layer that is needed for the input text to be converted to numbers.
     """
@@ -53,7 +57,7 @@ def create_vectorize_layer(lang_list):
         output_mode='int',
         output_sequence_length=sequence_length)
 
-    data_directory = "data"
+    data_directory = "data//huggingface"
     # load training data in to Pandas data frame
     train_df = pd.read_csv(os.path.join(data_directory, "train.csv"))
 
@@ -67,7 +71,7 @@ def create_vectorize_layer(lang_list):
     return vectorize_layer
 
 
-def _detect_language(model, vectorize_layer, lang_list):
+def _detect_language(model, vectorize_layer):
     """
     Detects the actual language of the input text.
     The input text itself is read from the session state.
@@ -121,7 +125,10 @@ def submit_feedback():
     # Open cursor to perform database operation
     cur = conn.cursor()
     postgres_insert_query = '''
-        INSERT INTO language_identification_feedback(text_from_user_input, language_by_classifier, probability_by_classifier, language_suggested_by_user)
+        INSERT INTO language_identification_feedback(text_from_user_input, 
+                                                    language_by_classifier, 
+                                                    probability_by_classifier, 
+                                                    language_suggested_by_user)
         VALUES (%s, %s, %s, %s )
     '''
     record_to_insert = (
@@ -144,12 +151,72 @@ def submit_feedback():
     st.session_state.ui_state = "render_feedback"
 
 
-def main():
-    lang_labels = {"es": "Spanish", "en": "English", "de": "German"}
-    lang_list = ["es", "en", "de"]
+def get_input():
+    # load pickled language model
+    language_model = load_model("simple_mlp_novectorize.h5")
+    # create the necessary vectorize layer for the text input
+    vectorize_layer = create_vectorize_layer()
 
+    st.text_area(
+        "Please enter sample text to detect language (use 20-50 words for best results):",
+        placeholder=None,
+        disabled=False,
+        key="text_input"
+    )
+
+    st.button(
+        label='Detect language',
+        on_click=_detect_language,
+        args=(language_model, vectorize_layer)
+    )
+
+
+def render_result():
+    st.text_area("Your entered text", value=st.session_state.text_input, disabled=True, key="text_input")
+
+    st.write(
+        f"""The classifier considers this text to be in {lang_labels[st.session_state.language]}. 
+        It is {round(float(st.session_state.probability) * 100, 1)}% sure.""")
+
+    st.subheader("Did it get it right?")
+
+    selected_language = st.selectbox('What is the real language? Please select in any case.',
+                                     ('Spanish', 'English', 'German'),
+                                     index=list(lang_labels.keys()).index(st.session_state.language))
+
+    st.session_state.lang_hint = list(lang_labels.keys())[list(lang_labels.values()).index(selected_language)]
+
+    st.write(
+        f"""You classify this text as {lang_labels[st.session_state.lang_hint]}.""")
+    if st.session_state.language == st.session_state.lang_hint:
+        st.write(f"""Looks like the classifier got it right. What a smart fellow!""")
+    else:
+        st.write(f"""Oh dear, there is still a long way to go for better training data and AI.""")
+
+    st.button('Submit feedback',
+              on_click=submit_feedback)
+
+
+def render_feedback():
+    st.subheader("Your feedback")
+
+    # use for debugging only
+    # st.write(
+    #     st.session_state.text_input,
+    #     st.session_state.language,
+    #     st.session_state.probability,
+    #     st.session_state.lang_hint
+    # )
+
+    st.success("Thank you for your feedback. This will improve the classifier.")
+    st.session_state.clear()
+    st.button('Start all over!')
+
+
+def main():
     # The next two UI elements are common for all screens
     st.title('SE4AI - Language Identification - Group 07')
+
     with st.sidebar:
         """
         This is a simple streamlit application that guesses the language of the text passed out of German, 
@@ -173,63 +240,11 @@ def main():
     ################################################################################################
 
     if st.session_state.ui_state == "get_input":
-        # load pickled language model
-        language_model = load_model("simple_mlp_novectorize.h5")
-        # create the necessary vectorize layer for the text input
-        vectorize_layer = create_vectorize_layer(lang_list)
-
-        st.text_area(
-            "Please enter sample text to detect language (use 20-50 words for best results):",
-            placeholder=None,
-            disabled=False,
-            key="text_input"
-        )
-
-        st.button(
-            label='Detect language',
-            on_click=_detect_language,
-            args=(language_model, vectorize_layer, lang_list)
-        )
-
+        get_input()
     elif st.session_state.ui_state == "render_result":
-        st.text_area("Your entered text", value=st.session_state.text_input, disabled=True, key="text_input")
-
-        st.write(
-            f"""The classifier considers this text to be in {lang_labels[st.session_state.language]}. 
-            It is {round(float(st.session_state.probability) * 100, 1)}% sure.""")
-
-        st.subheader("Did it get it right?")
-
-        selected_language = st.selectbox('What is the real language? Please select in any case.',
-                                         ('Spanish', 'English', 'German'),
-                                         index=list(lang_labels.keys()).index(st.session_state.language))
-
-        st.session_state.lang_hint = list(lang_labels.keys())[list(lang_labels.values()).index(selected_language)]
-
-        st.write(
-            f"""You classify this text as {lang_labels[st.session_state.lang_hint]}.""")
-        if st.session_state.language == st.session_state.lang_hint:
-            st.write(f"""Looks like the classifier got it right. What a smart fellow!""")
-        else:
-            st.write(f"""Oh dear, there is still a long way to go for better training data and AI.""")
-
-        st.button('Submit feedback',
-                  on_click=submit_feedback)
-
+        render_result()
     elif st.session_state.ui_state == "render_feedback":
-        st.subheader("Your feedback")
-
-        # use for debugging only
-        # st.write(
-        #     st.session_state.text_input,
-        #     st.session_state.language,
-        #     st.session_state.probability,
-        #     st.session_state.lang_hint
-        # )
-
-        st.success("Thank you for your feedback. This will improve the classifier.")
-        st.session_state.clear()
-        st.button('Start all over!')
+        render_feedback()
 
 
 if __name__ == "__main__":
