9e08fc105154637bd4316ca3634de106c7f360d9
ralf-koenig <ralf.koenig@studserv.uni-leipzig.de>
2022-05-24 03:20:09 +0200

    Simple test corrected

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ import pytest
 def test_load_model():
     # adding comment
     # test whether method returns None for empty string input
-    assert load_model('') == None
+    assert load_model('','') == None
 
 
 def test_not_implemented():
